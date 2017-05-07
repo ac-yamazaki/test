989c52eb4541d0a2b2f528b71e7c735675582085
ac-yamazaki <takashi.yamazaki@alpha-connection.net>
2017-05-07 22:47:15 +0900

    test

---
+++ tests.php
@@ -0,0 +1,23 @@
+<?php
+//use vendor\phpunit\phpunit\src\Framework\TestCase;
+
+//require_once('./index.php');
+class SampleTest extends TestCase
+{
+//    public function test()
+//     {
+//         $Sample = new Sample();
+//         $this->assertEquals($Sample->helloWorld(), 'hello world!');
+//     }
+
+    /**
+     * @test
+     */
+   public function testIndex()
+    {
+        $Sample = new Sample();
+        $this->assertEquals($Sample->helloWorld(), 'hello world!');
+
+    }
+}
+?>
